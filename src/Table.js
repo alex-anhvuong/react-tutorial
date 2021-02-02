79ed65d2ff44e4e382e43075308ccb62ce019a27
Alex Vuong <vavuong@deakin.edu.au>
2021-02-02 12:45:09 +1100

    first commit

---
+++ src/Table.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+const Table = (props) => {
+    const { characterData, removeCharacter } = props;
+
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    );
+}
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Full Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <button onClick={() => props.removeCharacter(index)}>Delete</button>
+            </tr>
+        );
+    });
+
+    return (
+        <tbody>{rows}</tbody>
+    )
+}
+
+export default Table
